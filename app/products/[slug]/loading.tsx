9d497691fe0536a2b76c1eb9fd4b7cc8b393a90f
Maddy2001dev <mehdi.bordbar80@gmail.com>
2024-05-11 21:21:33 +0330

    loading fix?

--- app/products/[slug]/loading.tsx
+++ app/products/[slug]/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loader() {
-  return <div>loading</div>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="Loading w-full"></div>;
+    </div>
+  );
 }
