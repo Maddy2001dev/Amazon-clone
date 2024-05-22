600818fab0bfca0c331f066ff74967b0739b2603
Maddy2001dev <mehdi.bordbar80@gmail.com>
2024-05-22 18:44:27 +0800

    navbar small fix

--- app/components/navbar/LanguageNav.tsx
+++ app/components/navbar/LanguageNav.tsx
@@ -26,147 +26,145 @@ export default function LanguageNav() {
 
   const Modal = (
     <>
-      <div className="absolute top-[50px] left-[67vw] z-[51]">
-        <div className="relative">
-          <div className="triangle absolute top-[-10px] left-[10.3vw]"></div>
-          <div className="bg-white rounded flex flex-col justify-center gap-1 p-2">
-            <div className="flex gap-[8px] items-center mb-4">
-              <p className="text-[13px] text-[#444]">Change language</p>
-              <Link
-                href="#"
-                className="text-[#007185] text-[13px] hover:text-red-600 hover:underline"
-              >
-                Learn more
-              </Link>
-            </div>
-            <ul className="text-[14px] flex flex-col gap-1">
-              <li className="flex gap-1">
-                <input
-                  type="radio"
-                  checked={lang === 'en'}
-                  onChange={(e) => handleChange(e)}
-                  value="en"
-                />
-                <p>English - EN</p>
-              </li>
-              <li className="flex gap-1">
-                <input
-                  onChange={(e) => handleChange(e)}
-                  type="radio"
-                  checked={lang === 'es'}
-                  value="es"
-                  className=""
-                />
-                <p>español - ES</p>
-              </li>
-              <li className="flex gap-1">
-                <input
-                  onChange={(e) => handleChange(e)}
-                  type="radio"
-                  checked={lang === 'ar'}
-                  value="ar"
-                  className=""
-                />
-                <p>العربية - AR</p>
-              </li>
-              <li className="flex gap-1">
-                <input
-                  onChange={(e) => handleChange(e)}
-                  type="radio"
-                  checked={lang === 'de'}
-                  value="de"
-                  className=""
-                />
-                <p>Deutsch - DE</p>
-              </li>
-              <li className="flex gap-1">
-                <input
-                  onChange={(e) => handleChange(e)}
-                  type="radio"
-                  checked={lang === 'he'}
-                  value="he"
-                  className=""
-                />
-                <p>עברית - HE</p>
-              </li>
-              <li className="flex gap-1">
-                <input
-                  onChange={(e) => handleChange(e)}
-                  type="radio"
-                  checked={lang === 'ko'}
-                  value="ko"
-                  className=""
-                />
-                <p>한국어 - KO</p>
-              </li>
-              <li className="flex gap-1">
-                <input
-                  onChange={(e) => handleChange(e)}
-                  type="radio"
-                  checked={lang === 'pt'}
-                  value="pt"
-                  className=""
-                />
-                <p>português - PT</p>
-              </li>
-              <li className="flex gap-1">
-                <input
-                  onChange={(e) => handleChange(e)}
-                  type="radio"
-                  checked={lang === 'zh'}
-                  value="zh"
-                  className=""
-                />
-                <p>中文 (简体) - ZH</p>
-              </li>
-            </ul>
-            <hr className="border-slate-400 h-[1px]" />
-            <div className="flex gap-[8px] items-center">
-              <p className="text-[13px] text-[#444]">Change currency</p>
-              <Link
-                href="#"
-                className="text-[#007185] text-[13px] hover:text-red-600 hover:underline"
-              >
-                Learn more
-              </Link>
-            </div>
-            <div className="flex justify-between items-cente text-[13px]">
-              <p>$ - USD - US Dollar</p>
-              <Link
-                href="#"
-                className="text-[#007185] text-[13px] hover:text-red-600 hover:underline"
-              >
-                Change
-              </Link>
-            </div>
-            <hr className="border-slate-400 h-[1px]" />
-            <div className="flex items-center gap-[2px]">
-              <Image
-                src="/images/icons/united-states.png"
-                width={25}
-                height={25}
-                alt="flag"
-              />
-              <p className="  ">you are shopping on Amazon.com</p>
-            </div>
+      <div className="absolute w-72 top-full -right-[calc(8rem-2px)] z-[51]">
+        <div className="triangle absolute top-[-10px] left-1/2 -translate-x-1/2"></div>
+        <div className="bg-white rounded flex flex-col justify-center gap-1 p-2">
+          <div className="flex gap-[8px] items-center mb-4">
+            <p className="text-[13px] text-[#444]">Change language</p>
             <Link
               href="#"
-              className="text-center mt-1 text-[#007185] hover:underline"
+              className="text-[#007185] text-[13px] hover:text-red-600 hover:underline"
             >
-              Change country/region.
+              Learn more
             </Link>
           </div>
+          <ul className="text-[14px] flex flex-col gap-1">
+            <li className="flex gap-1">
+              <input
+                type="radio"
+                checked={lang === 'en'}
+                onChange={(e) => handleChange(e)}
+                value="en"
+              />
+              <p>English - EN</p>
+            </li>
+            <li className="flex gap-1">
+              <input
+                onChange={(e) => handleChange(e)}
+                type="radio"
+                checked={lang === 'es'}
+                value="es"
+                className=""
+              />
+              <p>español - ES</p>
+            </li>
+            <li className="flex gap-1">
+              <input
+                onChange={(e) => handleChange(e)}
+                type="radio"
+                checked={lang === 'ar'}
+                value="ar"
+                className=""
+              />
+              <p>العربية - AR</p>
+            </li>
+            <li className="flex gap-1">
+              <input
+                onChange={(e) => handleChange(e)}
+                type="radio"
+                checked={lang === 'de'}
+                value="de"
+                className=""
+              />
+              <p>Deutsch - DE</p>
+            </li>
+            <li className="flex gap-1">
+              <input
+                onChange={(e) => handleChange(e)}
+                type="radio"
+                checked={lang === 'he'}
+                value="he"
+                className=""
+              />
+              <p>עברית - HE</p>
+            </li>
+            <li className="flex gap-1">
+              <input
+                onChange={(e) => handleChange(e)}
+                type="radio"
+                checked={lang === 'ko'}
+                value="ko"
+                className=""
+              />
+              <p>한국어 - KO</p>
+            </li>
+            <li className="flex gap-1">
+              <input
+                onChange={(e) => handleChange(e)}
+                type="radio"
+                checked={lang === 'pt'}
+                value="pt"
+                className=""
+              />
+              <p>português - PT</p>
+            </li>
+            <li className="flex gap-1">
+              <input
+                onChange={(e) => handleChange(e)}
+                type="radio"
+                checked={lang === 'zh'}
+                value="zh"
+                className=""
+              />
+              <p>中文 (简体) - ZH</p>
+            </li>
+          </ul>
+          <hr className="border-slate-400 h-[1px]" />
+          <div className="flex gap-[8px] items-center">
+            <p className="text-[13px] text-[#444]">Change currency</p>
+            <Link
+              href="#"
+              className="text-[#007185] text-[13px] hover:text-red-600 hover:underline"
+            >
+              Learn more
+            </Link>
+          </div>
+          <div className="flex justify-between items-cente text-[13px]">
+            <p>$ - USD - US Dollar</p>
+            <Link
+              href="#"
+              className="text-[#007185] text-[13px] hover:text-red-600 hover:underline"
+            >
+              Change
+            </Link>
+          </div>
+          <hr className="border-slate-400 h-[1px]" />
+          <div className="flex items-center gap-[2px]">
+            <Image
+              src="/images/icons/united-states.png"
+              width={25}
+              height={25}
+              alt="flag"
+            />
+            <p className="  ">you are shopping on Amazon.com</p>
+          </div>
+          <Link
+            href="#"
+            className="text-center mt-1 text-[#007185] hover:underline"
+          >
+            Change country/region.
+          </Link>
         </div>
       </div>
     </>
   );
 
   return (
-    <div className="hidden sm: md:block lg:block">
+    <div className="hidden sm: md:block lg:block relative">
       <div
         onMouseEnter={() => setShowModal(() => true)}
         onMouseLeave={() => setShowModal(() => false)}
-        className=" mr-[4px] cursor-pointer flex items-end justify-center gap-1.6 p-1 hover:border-[1px] hover:rounded-sm border-[1px] border-nav hover:border-white ml-3"
+        className=" mr-[5px] cursor-pointer flex items-end justify-center gap-1.6 p-1 hover:border-[1px] hover:rounded-sm border-[1px] border-nav hover:border-white ml-3"
       >
         <Image
           className="mt-1"
--- app/components/navbar/SignIn.tsx
+++ app/components/navbar/SignIn.tsx
@@ -19,115 +19,113 @@ export default function SignIn() {
 
   const Modal = (
     <>
-      <div className="absolute top-[56px] left-[60vw] z-[51]">
-        <div className="relative">
-          <div className="triangle absolute top-[-10px] left-[26.6vw]"></div>
-          <div className="bg-white rounded flex flex-col justify-center gap-1 p-2 w-[500px]">
-            <button className="bg-[#FFD814] hover:bg-yellow-500 border-yellow-400 shadow self-center w-[70%] shadow-slate-400 border-[1px] rounded-full px-3 py-1.6">
-              Sign in
-            </button>
-            <div className="text-[11px] self-center flex gap-1">
-              <p>New customer?</p>
-              <Link href="#" className="text-[#007185] hover:underline">
-                Start here.
+      <div className="absolute top-[102%] w-max -right-[80px] z-[51]">
+        <div className="triangle absolute top-[-10px] right-[80px]"></div>
+        <div className="bg-white rounded flex flex-col justify-center gap-1 p-2 w-[500px]">
+          <button className="bg-[#FFD814] hover:bg-yellow-500 border-yellow-400 shadow self-center w-[70%] shadow-slate-400 border-[1px] rounded-full px-3 py-1.6">
+            Sign in
+          </button>
+          <div className="text-[11px] self-center flex gap-1">
+            <p>New customer?</p>
+            <Link href="#" className="text-[#007185] hover:underline">
+              Start here.
+            </Link>
+          </div>
+          <hr className="border-slate-200 h-[1px]" />
+          <div className="p-1.6 flex justify-between ">
+            <div className="border-r-slate-200 flex flex-col border-r-[1px] w-[50%] p-1">
+              <Link href="#" className="font-[700] text-[16px]">
+                Your Lists
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Create a List
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Find a List or Registry
               </Link>
             </div>
-            <hr className="border-slate-200 h-[1px]" />
-            <div className="p-1.6 flex justify-between ">
-              <div className="border-r-slate-200 flex flex-col border-r-[1px] w-[50%] p-1">
-                <Link href="#" className="font-[700] text-[16px]">
-                  Your Lists
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Create a List
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Find a List or Registry
-                </Link>
-              </div>
-              <div className="w-[50%] flex flex-col p-1">
-                <Link href="#" className="font-[700] text-[16px]">
-                  Your Account
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Account
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  orders
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Recommendations
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Browsing
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  History
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Watchlist
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Video
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Purchases & Rentals
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Kindle Unlimited
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Subscribe & Save Items
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Memberships & Subscriptions
-                </Link>
-                <Link
-                  href="#"
-                  className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
-                >
-                  Music Library
-                </Link>
-              </div>
+            <div className="w-[50%] flex flex-col p-1">
+              <Link href="#" className="font-[700] text-[16px]">
+                Your Account
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Account
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                orders
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Recommendations
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Browsing
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                History
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Watchlist
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Video
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Purchases & Rentals
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Kindle Unlimited
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Subscribe & Save Items
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Memberships & Subscriptions
+              </Link>
+              <Link
+                href="#"
+                className="text-[#444] cursor-pointer text-[13px] hover:underline hover:text-[#e47911]"
+              >
+                Music Library
+              </Link>
             </div>
           </div>
         </div>
@@ -139,7 +137,7 @@ export default function SignIn() {
     <div
       onMouseEnter={() => setShowModal(() => true)}
       onMouseLeave={() => setShowModal(() => false)}
-      className="text-nowrap hidden sm:hidden md:block lg:block mr-1.4 p-1.6 cursor-pointer hover:border-[1px] hover:rounded-sm border-[1px] border-nav hover:border-white"
+      className="relative text-nowrap hidden sm:hidden md:block lg:block mr-1.4 p-1.6 cursor-pointer hover:border-[1px] hover:rounded-sm border-[1px] border-nav hover:border-white"
     >
       <div className="text-white">
         <p className="text-[12px]">Hello, sign in</p>
